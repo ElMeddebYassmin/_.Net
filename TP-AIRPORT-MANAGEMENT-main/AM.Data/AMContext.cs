865df845aa5052c8377fb8dd1f3fef073598c69d
Yassmin Mouaddeb <98842040+ElMeddebYassmin@users.noreply.github.com>
2023-03-06 12:15:22 +0100

    Entity Framework Core

---
+++ TP-AIRPORT-MANAGEMENT-main/AM.Data/AMContext.cs
@@ -0,0 +1,23 @@
+﻿using AM.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AM.Data
+{
+    public class AMContext : DbContext
+    {
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Traveller> Travellers { get; set; }
+
+        //parametrer l'acces a la bd
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb; 
+                    Initial Catalog = Airport;  
+                    Integrated Security = true");
+         
+        }
+    }
+}
